4801da2a66d4e1bc1bc6880df4e96b560ea63fd6
BraulioOsorio <braulioosoriomartinez@gmail.com>
2024-08-23 11:02:46 -0500

    user no h

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -92,7 +92,7 @@ export const find_user = async (req, res) => {
 export const find_user_name = async (req, res) => {
     try {
         const user = await prisma.users.findFirst({ where: { network_user: {contains: req.params.network_user,mode: 'insensitive'} } });
-        if (!user) {return res.status(404).json({ error: 'Usuario no encontrado' })}
+        if (!user) {return 'Usuario no encontrado'}
         res.json(user);
     } catch (error) {
         console.error('Error find user:', error);
