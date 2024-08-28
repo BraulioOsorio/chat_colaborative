cc79feddbb7d62aa10f66f831ba764d2f6b9ebbe
BraulioOsorio <braulioosoriomartinez@gmail.com>
2024-08-28 08:08:34 -0500

    ms

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const messageInput = document.getElementById('messageInput');
 
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF91c2VyIjoiYzFhOWY0NWEtNDUyMS00IiwiaWF0IjoxNzI0ODQ5NTExLCJleHAiOjE3MjQ4NzExMTF9.KCuOZOsIbHpNTyIGdX11yf7TN3kVtc1-rreheSbVXp4'; // Reemplaza con el token JWT válido
-    const userId = '002a3f24-d18d-4cd6-83b1-1d67a0ed069'; // Reemplaza con el ID del usuario
+    const userId = 'c1a9f45a-4521-4'; // Reemplaza con el ID del usuario
+    const rescipient_id = "413553d5-03a1-4"
 
     // Conectar al WebSocket
     const socket = io('https://intern-chat-backend-production-uy3j.onrender.com/');
 
     // Unirse al canal de mensajes directos
-    socket.emit('direct_message', {recipient_id: userId, token });
+    socket.emit('direct_message', {send_id : rescipient_id ,recipient_id: userId, token });
 
     // Escuchar mensajes de la conversación
     socket.on('get_direct_messages', (messages) => {
