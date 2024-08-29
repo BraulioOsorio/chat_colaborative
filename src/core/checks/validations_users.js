3040f9fe508791c8e1a34a087f51e6652dfea994
axwsr <mrlibertadfinanciera09@gmail.com>
2024-08-29 09:45:59 -0500

    ready simbolos

--- src/core/checks/validations_users.js
+++ src/core/checks/validations_users.js
@@ -44,6 +44,7 @@ export const find_user_validation = [
 
 export const find_user_name_validation = [
   param('network_user').notEmpty().withMessage('El usuario de red no puede estar vacío')
+  .isAlphanumeric().withMessage('El usuario de red solo puede contener letras y números')
 ];
 
 export const delete_user_validation = [
