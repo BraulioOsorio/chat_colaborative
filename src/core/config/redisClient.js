4508ec0a7ad1819c0684c2576c8f1887242cb321
Midnightgb <midnight3424@gmail.com>
2024-09-04 20:03:46 -0500

    chore: Update Redis configuration and connection

--- src/core/config/redisClient.js
+++ src/core/config/redisClient.js
@@ -1,13 +1,11 @@
 import { createClient } from 'redis';
-
-// URL de Redis proporcionada por Render
-const redisUrlRender = 'rediss://red-crc63bqj1k6c738hcpk0:A05cKBHRMt2R7iN5Xvn9MFLVsjashs0q@oregon-redis.render.com:6379';
+import { REDIS_PASSWORD, REDIS_HOST, REDIS_PORT } from './config.js';
 
 const redisClient = createClient({
-    password: 'N1bHkywu5IKl87rXDeyXFWBKxCxemJ02',
+    password: REDIS_PASSWORD,
     socket: {
-        host: 'redis-10380.c281.us-east-1-2.ec2.redns.redis-cloud.com',
-        port: 10380
+        host: REDIS_HOST,
+        port: REDIS_PORT
     }
 });
 // Manejar errores de conexión
@@ -23,5 +21,4 @@ const connectRedis = async () => {
     }
 };
 
-// Exportar el cliente y la función de conexión
 export { redisClient, connectRedis };
