d7265a02a5c8a65d71a52bf03c0f9748788b0e07
Midnightgb <midnight3424@gmail.com>
2024-10-11 19:18:27 -0500

    Subject: Extend access token expiry time
    Increased the expiration time for generated access tokens from 7 minutes to 1 hour to provide users with longer uninterrupted session durations.

--- src/core/config/utils.js
+++ src/core/config/utils.js
@@ -17,7 +17,7 @@ export const generate_user_id = (length) => {
 }
 
 export const create_access_token = (user_id,status) => {
-    const jwt_token = jwt.sign({ id_user: user_id }, SECRET_KEY, { expiresIn: '7m' })
+    const jwt_token = jwt.sign({ id_user: user_id }, SECRET_KEY, { expiresIn: '1h' })
     if(status){register_token(jwt_token,user_id)}
     return jwt_token
 }
