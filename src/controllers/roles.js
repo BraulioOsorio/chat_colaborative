530297d37f7dc7c4ebeaf86cbe87ef8fc6ca8aa9
Midnightgb <midnight3424@gmail.com>
2024-10-16 18:42:51 -0500

    feat: Enhance role retrieval endpoint to include associated users and permissions
    The endpoint now returns a more comprehensive data structure that includes not only the roles themselves but also their associated users and permissions. This richer information offers greater insight into the system's authorization model, proving valuable for administrative tasks, debugging, and potential future feature development.

--- src/controllers/roles.js
+++ src/controllers/roles.js
@@ -3,14 +3,31 @@ import {  cacheData, getCachedData } from '../core/config/utils.js';
 
 export const get_roles = async (req, res) => {
     try {
-        if (req.user.role.name !== "SUPERADMIN") {return res.status(403).json({ error: 'El usuario no tiene permisos' })}
+        if (req.user.role.name !== "SUPERADMIN") {
+            return res.status(403).json({ error: 'El usuario no tiene permisos' });
+        }
+        
         const cachedRoles = await getCachedData(`roles`);
         if (cachedRoles) {
             return res.json(cachedRoles);
         }
-        const role_consult = await prisma.roles.findMany({include:{ role_permission:{select:{Permissions:true} }}});
+        
+        const role_consult = await prisma.roles.findMany({
+            include: {
+                users: {
+                    include: {
+                        role_permission: {
+                            include: {
+                                Permissions: true
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        
         await cacheData(`roles`, role_consult);
-        return res.json(role_consult)
+        return res.json(role_consult);
     } catch (error) {
         console.error('Error get roles:', error);
         return res.status(500).json({ error: 'Internal Server Error get_roles' });
